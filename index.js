8d6c305bc656954dfa78b9e6f1de8f32e54c2d15
unknown <gyash.21decgmail.com>
2023-03-23 11:07:29 +0530

    created server and added folders and a home controller with router

---
+++ index.js
@@ -0,0 +1,14 @@
+const express= require('express');
+const app= express();
+const port=8000;
+
+app.use('/', require('./routes'));
+
+app.listen(port, function(err){
+    if(err){
+        console.log("Error in establishing server");
+        return;
+    }
+
+    console.log("connected to Server:",port);
+})

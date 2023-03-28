0af053a239b923d096afa6b79e71e381826ac884
unknown <gyash.21decgmail.com>
2023-03-28 21:08:38 +0530

    added sign up and new user's data to databases with a controller named createUser

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.signIn= function(req,res){
 
     return res.render('users_sign_in',
@@ -6,3 +8,34 @@ module.exports.signIn= function(req,res){
         title: "Sign In"
     })
 }
+
+
+module.exports.createUser =async function(req,res){
+        if(req.body.password != req.body.confirm_password){
+            return res.redirect('back');
+        }
+        delete req.body.confirm_password;
+    //    User.create(req.body, function(err, user){
+    //     if(err){console.log("Error in creating User"); return;}
+
+    //     console.log("User added");
+    //     console.log(user);
+    //     return res.redirect('/users/sign-in');
+
+    //    })
+
+    const existUser = await User.findOne({email:req.body.email});
+
+    if(!existUser){
+        try{
+            const createdUser = await  User.create(req.body);
+            console.log(createdUser);
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+
+        return res.redirect('/users/sign-in');
+}
+
--- routes/users.js
+++ routes/users.js
@@ -3,4 +3,6 @@ const router= express.Router();
 const userController= require('../controllers/users_controller');
 router.get('/sign-in', userController.signIn);
 
+router.post('/create-User', userController.createUser);
+
 module.exports= router;
